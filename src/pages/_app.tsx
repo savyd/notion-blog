a0d1570c56485968c5b613b54bdca20d1dcc417e
sayid_haidar <sayyid.abdul.aziz.haidar@gmail.com>
2020-07-13 04:57:03 +0700

    update navbar, contact, and footer

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,3 @@
 import '../styles/global.css'
-import Footer from '../components/footer'
 
-export default ({ Component, pageProps }) => (
-  <>
-    <Component {...pageProps} />
-    <Footer />
-  </>
-)
+export default ({ Component, pageProps }) => <Component {...pageProps} />
--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -13,22 +13,22 @@ const contacts = [
   {
     Comp: Twitter,
     alt: 'twitter icon',
-    link: 'https://twitter.com/_ijjk',
+    link: 'https://twitter.com/SayidHaidar1',
   },
   {
     Comp: GitHub,
     alt: 'github icon',
-    link: 'https://github.com/ijjk',
+    link: 'https://github.com/savyd',
   },
   {
     Comp: LinkedIn,
     alt: 'linkedin icon',
-    link: 'https://www.linkedin.com/in/jj-kasper-0b5392166/',
+    link: 'https://www.linkedin.com/in/sayyid-abdul-aziz-3a9230146/',
   },
   {
     Comp: Envelope,
     alt: 'envelope icon',
-    link: 'mailto:jj@jjsweb.site?subject=Notion Blog',
+    link: 'mailto:hello@sayid.id?subject=Notion Blog',
   },
 ]
 
@@ -43,8 +43,8 @@ export default () => (
       <h1 style={{ marginTop: 0 }}>Contact</h1>
 
       <div className={contactStyles.name}>
-        JJ Kasper - Next.js Engineer @{' '}
-        <ExtLink href="https://vercel.com">Vercel</ExtLink>
+        Sayyid Haidar - Backend Engineer @{' '}
+        <ExtLink href="https://skydu.id">Skydu</ExtLink>
       </div>
 
       <div className={contactStyles.links}>
